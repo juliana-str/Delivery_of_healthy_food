85141bd44630c59e8402bca2bd5852d22d7a521e
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-18 11:16:14 +0300

    add orders

---
+++ delivery_healthy_food/orders/models.py
@@ -0,0 +1,78 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+from users.models import User
+from products.models import Product
+
+
+STATUS = ['В обработке', 'Комплектуется' 'Собран',
+          'Передан в доставку', 'Доставлен', 'завершен']
+
+PAYMENT_METHODS = ['Наличные', 'Картой на сайте', 'При получении']
+
+DELIVERY_METHOD = ['Пункт выдачи', 'Курьером']
+
+
+class ShoppingCart(models.Model):
+    """Model for creating a shopping cart."""
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='shopping_carts',
+        verbose_name='Добавил в корзину'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='shopping_carts',
+        verbose_name='Продукт в корзине'
+    )
+    quantity = models.IntegerField(
+        verbose_name='Количество товара',
+        validators=[
+                MinValueValidator(1, 'Разрешены значения от 1 до 100'),
+                MaxValueValidator(10000, 'Разрешены значения от 1 до 100')
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'product'],
+                name='unique_shopping_cart'
+            )
+        ]
+
+class Order(models.Model):
+    """Model for creating an order."""
+    customer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='orders',
+        verbose_name='Покупатель'
+    )
+    goods = models.ForeignKey(
+        ShoppingCart,
+        on_delete=models.CASCADE,
+        related_name='orders',
+        verbose_name='Покупки'
+    )
+    date = models.DateField(
+        verbose_name='Дата оформления',
+        auto_now_add=True
+    )
+    status = models.TextChoices(STATUS)
+    payment_method = models.TextChoices(PAYMENT_METHODS)
+    is_paid = models.BooleanField(default=False)
+    comment = models.TextField(
+        max_length=400,
+        null=True
+    )
+    delivery_method = models.TextChoices(DELIVERY_METHOD)
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
